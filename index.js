84e6602df2e7b0308467ebc7ae021ee3ab447360
Eojtir <eojtir@gmail.com>
2023-03-01 16:38:22 -0500

    practica-reto-09

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const jsConfetti = new JSConfetti();
+
+jsConfetti.addConfetti({
+  emojis: ["#", "$", "⭐️"],
+  confettiNumber: 100,
+});
+
 // Esta es formar en la que JS accede a los elementos
 
 // es el div donde estan todos nuestros cards
